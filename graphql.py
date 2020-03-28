532918dd4aa14da7924900dd39b23406d81001d9
zachary <syyun@snu.ac.kr>
2020-03-28 13:24:08 +0900

    Add gradcam sender

--- graphql.py
+++ graphql.py
@@ -5,6 +5,9 @@ import json
 
 import numpy as np
 
+from tqdm import tqdm
+
+
 from invokabilities import generate_sorted_dict_for_invokabilities
 import pickle
 
@@ -25,6 +28,8 @@ available_ds = [2, 18, 22, 31, 34, 46, 56, 58, 60, 62, 67, 68, 69, 75, 76,
                 453, 454, 456, 457, 461, 464, 468, 471, 472, 473, 475, 476,
                 477, 479, 480, 482, 483, 484, 485, 486, 488, 490, 492, 493,
                 495, 499, 504, 505, 513, 518, 523]
+
+available_articles = ['Article I', 'Article I:1', 'Article II', 'Article II:1', 'Article II:1(a)', 'Article II:1(b)', 'Article II:2', 'Article II:3', 'Article III', 'Article III:1', 'Article III:2', 'Article III:4', 'Article III:5', 'Article III:7', 'Article IV', 'Article IX', 'Article IX:2', 'Article V', 'Article V:1', 'Article V:2', 'Article V:3', 'Article V:3(a)', 'Article V:4', 'Article V:5', 'Article V:6', 'Article V:7', 'Article VI', 'Article VI:1', 'Article VI:2', 'Article VI:2(a)', 'Article VI:2(b)', 'Article VI:3', 'Article VI:5(a)', 'Article VI:6', 'Article VII', 'Article VII:1', 'Article VII:2', 'Article VII:5', 'Article VIII', 'Article VIII:1', 'Article VIII:3', 'Article VIII:4', 'Article X', 'Article X:1', 'Article X:2', 'Article X:3', 'Article X:3(a)', 'Article XI', 'Article XI:1', 'Article XIII', 'Article XIII:1', 'Article XIII:2', 'Article XIII:3(b)', 'Article XIX', 'Article XIX:1', 'Article XIX:2', 'Article XIX:3', 'Article XV', 'Article XVI', 'Article XVI:1', 'Article XVI:4', 'Article XVII', 'Article XVII:1', 'Article XVII:1(c)', 'Article XVIII', 'Article XVIII:10', 'Article XVIII:11', 'Article XX', 'Article XXI', 'Article XXII', 'Article XXII:1', 'Article XXIII', 'Article XXIII:1', 'Article XXIII:1(a)', 'Article XXIII:1(b)', 'Article XXIV', 'Article XXIV:12', 'Article XXIV:5(b)', 'Article XXIV:6', 'Article XXVIII']
 
 class AppSyncClient:
     latest_version = "1.0.0"
@@ -124,6 +129,57 @@ class AppSyncClient:
         print(res)
         return res["data"]["createGovGradCAM"]
 
+    def create_article_tokens(self, article: int, tokens: List[str], version: str = "1.0.0"):
+        article = '"{}"'.format(article)
+        version = '"{}"'.format(version)
+        tokens = '{}'.format(tokens)
+        query = """
+                      mutation CreateArticleTokenized {{
+                          createArticleTokenized(input: 
+                              {{
+                              article: {0},
+                              version: {1},
+                              tokens: {2}
+                              }}
+                              ) {{
+                                     article
+                                     version
+                                     tokens
+                                  }}
+                              }}
+                      """.format(
+            article, version, tokens
+        )
+        res = self.execute_gql(query).json()
+        print(res)
+        return res["data"]["createArticleTokenized"]
+
+    def create_article_gradcam(self, ds_art: str, weights: List[float], version: str = "1.0.0"):
+        ds_art = '"{}"'.format(ds_art)
+        version = '"{}"'.format(version)
+        weights = '{}'.format(weights)
+        query = """
+                   mutation CreateArticleGradCam {{
+                       createArticleGradCAM(input: 
+                           {{
+                           ds_art: {0}, 
+                           version: {1},
+                           weights: {2}
+                           }}
+                           ) {{
+                                  ds_art
+                                  version
+                                  weights
+                               }}
+                           }}
+                   """.format(
+            ds_art, version, weights
+        )
+
+        res = self.execute_gql(query).json()
+        print(res)
+        return res["data"]["createArticleGradCAM"]
+
 
 if __name__ == "__main__":
     client = AppSyncClient(api_key="da2-ojotqixqrff5tdaarm7zpn5cfa",
@@ -141,12 +197,31 @@ if __name__ == "__main__":
     #     res = client.create_gov_tokens(ds=ds, tokens=tokens)
     #     print(res)
 
+    # fp = 'result_dict_train.pkl'
+    # data = load_pickle(fp)
+    # keys = list(data.keys())
+    #
+    # for i, key in enumerate(keys):
+    #     print(i)
+    #     print(key)
+    #     grad_cam_gov = list(data[key]['grad_cam_gov'])
+    #     res = client.create_gov_gradcam(ds_art=key, weights=grad_cam_gov)
+    #     print(res)
+
+
+    # fp = 'article_dict_tokenized.pkl'
+    # data = load_pickle(fp)
+    # print(len(available_articles))
+    # for article in available_articles:
+    #     tokens = json.dumps(data[article])
+    #     res = client.create_article_tokens(article=article, tokens=tokens)
+
     fp = 'result_dict_test.pkl'
     data = load_pickle(fp)
     keys = list(data.keys())
 
-    for key in keys:
+    for i, key in tqdm(enumerate(keys)):
         print(key)
-        grad_cam_gov = list(data[key]['grad_cam_gov'])
-        res = client.create_gov_gradcam(ds_art=key, weights=grad_cam_gov)
+        grad_cam_art = list(data[key]['grad_cam_art'])
+        res = client.create_article_gradcam(ds_art=key, weights=grad_cam_art)
         print(res)
